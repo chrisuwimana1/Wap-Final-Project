0e6a1fb0c8d91a7353ba645d49088032a1559aa3
sina15 <2038659554@qq.com>
2019-04-22 16:27:11 -0500

    all changes

--- src/main/webapp/js/team.js
+++ src/main/webapp/js/team.js
@@ -1,6 +1,22 @@
-// $.get("getTeam",{dataType:"application/json"})
-//                   .done(function (data) {
-//                       alert(data)
-//                   })
-//                   .error()
-//                     .always()
+$(function () {
+    alert("hello me")
+    $.get("getTeam",{dataType:"application/json"})
+        .done(function (data) {
+            $("#TeamListTableBody").empty();
+             console.log(data)
+            for (let i=0;i<data.length;i++){
+
+               $("#TeamListTableBody").prepend("<tr>\n" +
+                   "            <td>"+data[i].id+"</td>\n" +
+                   "            <td>"+data[i].name+"</td>\n" +
+                   "            <td>"+data[i].description+"</td>\n" +
+                   "        </tr>")
+            }
+        })
+        .error(function () {
+            alert("failure")
+        })
+        .always();
+
+
+})
---
+++ src/main/webapp/team1.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: christophehabineza
+  Date: 2019-04-22
+  Time: 14:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script>
+        alert("hello ")
+    </script>
+</head>
+<body>
+
+</body>
+</html>
