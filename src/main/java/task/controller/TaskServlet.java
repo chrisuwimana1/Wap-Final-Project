ce2895dd1107df8b195b77c1cbe0bc5f54a11ba0
Celestin Mbuyamba <celembuyamba@gmail.com>
2019-04-22 14:52:00 -0500

    Task Servlet Changes

--- src/main/java/task/controller/TaskServlet.java
+++ src/main/java/task/controller/TaskServlet.java
@@ -8,7 +8,11 @@ package task.controller;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,25 +36,34 @@ public class TaskServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         List<Task> allTasks = TaskService.getAllTasks();
-        
-        List<Task> list = new TaskMngtDao<Task>().executeNativeQuery("select * from TASK",Task.class);
-        
-       
-        
-        
-        for (Task task : list) {
-            task.getProjectManagerId().getUserRoleList().clear();
-            task.getTaskOwnerId().getUserRoleList().clear();
-        }
-        
+
+        List<Task> list = new TaskMngtDao<Task>().executeNativeQuery("select * from TASK", Task.class);
+
+        List<Map> newList = new ArrayList<>();
+
+        list.stream().map((task) -> {
+            Map<String, Object> map = new HashMap();
+            map.put("name", task.getName());
+            map.put("category", task.getCategoryId().getName());
+            map.put("taskOwnerId", task.getCategoryId().getName());
+            map.put("projectManagerId", task.getCategoryId().getName());
+            map.put("priority", task.getPriority());
+            map.put("status", task.getStatus());
+            map.put("dueDate", task.getDueDate() == null ? "" : new SimpleDateFormat("MM-dd-yyyy").format(task.getDueDate()));
+            map.put("description", task.getDescription());
+            map.put("creationDate", task.getCreationDate() == null ? "" : new SimpleDateFormat("MM-dd-yyyy").format(task.getCreationDate()));
+            return map;
+        }).forEachOrdered((map) -> {
+            newList.add(map);
+        });
+
         System.out.println("allTasks = " + allTasks);
-        String json = new Gson().toJson(list);
+        String json = new Gson().toJson(newList);
         try (PrintWriter out = response.getWriter()) {
             response.setContentType("application/json");
             out.write(json);
